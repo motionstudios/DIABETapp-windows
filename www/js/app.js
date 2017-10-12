426b1f4e5f83ae643ea4933e39942d26ea7b2238
franciscorubiales <info@motionstudios.es>
2017-10-12 09:07:52 +0200

    app.js

--- www/js/app.js
+++ www/js/app.js
@@ -9,21 +9,8 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 
         .run(function ($ionicPlatform) {
             $ionicPlatform.ready(function () {
-                $ionicPlatform.views.swipeBackEnabled(false);
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                    cordova.plugins.Keyboard.disableScroll(true);
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault(true);
 
-                }
-                if (ionic.Platform.isAndroid()) {
-                    ionic.Platform.isFullScreen = false;
-                }
+
             });
         });
 
